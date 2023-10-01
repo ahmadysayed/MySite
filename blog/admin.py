53dc1e6aeb8cfb013286673bb3bf43b5439de5d2
ahmadysayed <ahmadysayedmujtaba@gmail.com>
2023-10-01 12:13:31 +0200

    Comment Admin

--- blog/admin.py
+++ blog/admin.py
@@ -10,7 +10,10 @@ class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
 
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user_name", "post")
+
 admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
-admin.site.register(Comment)
+admin.site.register(Comment, CommentAdmin)
